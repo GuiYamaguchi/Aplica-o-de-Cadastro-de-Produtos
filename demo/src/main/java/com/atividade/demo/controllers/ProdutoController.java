ace3050a82a1c42360f3351539972d7c57674f95
Guilherme <japajao25@gmail.com>
2026-03-12 21:59:16 -0300

    controller e service finalizado

--- demo/src/main/java/com/atividade/demo/controllers/ProdutoController.java
+++ demo/src/main/java/com/atividade/demo/controllers/ProdutoController.java
@@ -1,4 +1,41 @@
 package com.atividade.demo.controllers;
 
+import com.atividade.demo.models.ProdutoModel;
+import com.atividade.demo.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/produtos")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping
+    public ProdutoModel criar(@RequestBody ProdutoModel produtoModel){
+        return produtoService.criar(produtoModel);
+    }
+
+    @GetMapping
+    public List<ProdutoModel> buscarTodosProdutos(){
+        return produtoService.buscarTodosProdutos();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        produtoService.deletar(id);
+    }
+
+    @PutMapping
+    public ProdutoModel atualizar(@PathVariable Long id, @RequestBody ProdutoModel produtoModel){
+        return  produtoService.atualizar(id, produtoModel);
+    }
+
+
 }
--- demo/src/main/java/com/atividade/demo/services/ProdutoService.java
+++ demo/src/main/java/com/atividade/demo/services/ProdutoService.java
@@ -1,4 +1,36 @@
 package com.atividade.demo.services;
 
+import com.atividade.demo.models.ProdutoModel;
+import com.atividade.demo.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoModel criar(ProdutoModel produtoModel){
+        return produtoRepository.save(produtoModel);
+    }
+
+    public List<ProdutoModel> buscarTodosProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    public void deletar(Long id){
+        produtoRepository.deleteById(id);
+    }
+
+    public ProdutoModel atualizar(Long id, ProdutoModel produtoModel){
+        ProdutoModel newProduto = produtoRepository.findById(id).get();
+
+        newProduto.setNome(produtoModel.getNome());
+
+        return produtoRepository.save(newProduto);
+    }
 }
